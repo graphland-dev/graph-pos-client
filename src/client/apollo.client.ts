a839d60f6b977141366f2e193d02b34b2f0a183e
NishatFariza <nishatfarzia78@gmail.com>
2023-09-10 14:37:16 +0600

    setup apollo client

---
+++ src/client/apollo.client.ts
@@ -0,0 +1,8 @@
+// import { ApolloClient, InMemoryCache } from "@apollo/client";
+// import  {env}  from "@/client/apollo.client.ts";
+
+
+// export const apolloClient = new ApolloClient({
+//   uri: `${env.NEXT_PUBLIC_API_URL}/graphql`,
+//   cache: new InMemoryCache(),
+// });
