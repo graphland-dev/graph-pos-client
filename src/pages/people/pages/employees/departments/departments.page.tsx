66e72d89fa8ba98365ebb7406bbeb68ad91bedbd
KingRayhan <rayhan095@gmail.com>
2023-09-27 21:36:21 +0600

    fix update department dto

--- src/pages/people/pages/employees/departments/departments.page.tsx
+++ src/pages/people/pages/employees/departments/departments.page.tsx
@@ -1,105 +1,139 @@
 import {
-	EmployeeDepartment,
-	EmployeeDepartmentWithPagination,
-	MatchOperator,
-} from '@/_app/graphql-models/graphql';
-import { useMutation, useQuery } from '@apollo/client';
-import { Button, Drawer, Flex, Skeleton, Space, Title } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
-import { IconPlus } from '@tabler/icons-react';
-import { useState } from 'react';
-import DepartmentCard from './components/DepartmentCard';
-import DepartmentForm from './components/DepartmentForm';
+  EmployeeDepartment,
+  EmployeeDepartmentWithPagination,
+  MatchOperator,
+} from "@/_app/graphql-models/graphql";
+import { useMutation, useQuery } from "@apollo/client";
 import {
-	GET_EMPLOYEE_DEPARTMENT,
-	REMOVE_EMPLOYEE_DEPARTMENT,
-} from './utils/query';
+  ActionIcon,
+  Button,
+  Drawer,
+  Flex,
+  Skeleton,
+  Space,
+  Title,
+} from "@mantine/core";
+import cls from "classnames";
+import { useDisclosure } from "@mantine/hooks";
+import { IconPlus, IconRefresh } from "@tabler/icons-react";
+import { useState } from "react";
+import DepartmentCard from "./components/DepartmentCard";
+import DepartmentForm from "./components/DepartmentForm";
+import {
+  GET_EMPLOYEE_DEPARTMENT,
+  REMOVE_EMPLOYEE_DEPARTMENT,
+} from "./utils/query";
 
 const Departments = () => {
-	const [openedDrawer, drawerHandler] = useDisclosure();
-	const [selectedDepartment, onSelectDepartment] =
-		useState<EmployeeDepartment>();
-	const [action, setAction] = useState<'CREATE' | 'EDIT'>();
+  const [openedDrawer, drawerHandler] = useDisclosure();
+  const [selectedDepartment, onSelectDepartment] =
+    useState<EmployeeDepartment>();
+  const [action, setAction] = useState<"CREATE" | "EDIT">();
+  const [isRefetching, setIsRefetching] = useState(false);
 
-	const { data, loading, refetch } = useQuery<{
-		people__employeeDepartments: EmployeeDepartmentWithPagination;
-	}>(GET_EMPLOYEE_DEPARTMENT);
+  const { data, loading, refetch } = useQuery<{
+    people__employeeDepartments: EmployeeDepartmentWithPagination;
+  }>(GET_EMPLOYEE_DEPARTMENT);
 
-	const [removeDepartment] = useMutation(REMOVE_EMPLOYEE_DEPARTMENT, {
-		onCompleted: refetch,
-	});
+  const handleOnRefetch = () => {
+    setIsRefetching(true);
+    refetch().finally(() => {
+      setIsRefetching(false);
+    });
+  };
 
-	return (
-		<div>
-			<Flex justify={'space-between'} align={'center'}>
-				<Title order={2}>Employee Departments</Title>
-				<Button
-					leftIcon={<IconPlus size={16} />}
-					onClick={() => {
-						drawerHandler.open();
-						setAction('CREATE');
-					}}
-				>
-					Add new
-				</Button>
-			</Flex>
+  const [removeDepartment, { loading: deleting }] = useMutation(
+    REMOVE_EMPLOYEE_DEPARTMENT,
+    {
+      onCompleted: handleOnRefetch,
+    }
+  );
 
-			<Space h={50} />
+  return (
+    <div>
+      <Flex justify={"space-between"} align={"center"}>
+        <Title order={2}>Employee Departments</Title>
+        <Flex gap={"md"}>
+          <ActionIcon
+            onClick={() => refetch()}
+            variant="outline"
+            radius={100}
+            size={"lg"}
+          >
+            <IconRefresh
+              className={cls({
+                "animate-reverse-spin": loading || deleting || isRefetching,
+              })}
+            />
+          </ActionIcon>
+          <Button
+            leftIcon={<IconPlus size={16} />}
+            onClick={() => {
+              drawerHandler.open();
+              setAction("CREATE");
+            }}
+          >
+            Add new
+          </Button>
+        </Flex>
+      </Flex>
 
-			<div className='grid grid-cols-3 gap-4'>
-				{data?.people__employeeDepartments?.nodes?.map((department, idx) => (
-					<DepartmentCard
-						key={idx}
-						drawerHandler={drawerHandler}
-						onSelectDepartment={() => onSelectDepartment(department)}
-						onAction={() => setAction('EDIT')}
-						departmentData={department}
-						onRemove={() =>
-							removeDepartment({
-								variables: {
-									where: {
-										key: '_id',
-										operator: MatchOperator.Eq,
-										value: department?._id,
-									},
-								},
-							})
-						}
-					/>
-				))}
+      <Space h={50} />
 
-				{loading && (
-					<>
-						{' '}
-						{new Array(12).fill(12).map((_, idx: number) => (
-							<Skeleton key={idx} h={250} radius={10} />
-						))}
-					</>
-				)}
-			</div>
+      <div className="grid grid-cols-3 gap-4">
+        {data?.people__employeeDepartments?.nodes?.map((department, idx) => (
+          <DepartmentCard
+            key={idx}
+            drawerHandler={drawerHandler}
+            onSelectDepartment={() => onSelectDepartment(department)}
+            onAction={() => setAction("EDIT")}
+            departmentData={department}
+            onRemove={() => {
+              removeDepartment({
+                variables: {
+                  where: {
+                    key: "_id",
+                    operator: MatchOperator.Eq,
+                    value: department?._id,
+                  },
+                },
+              });
+            }}
+          />
+        ))}
 
-			<Drawer
-				opened={openedDrawer}
-				onClose={() => drawerHandler.open()}
-				position='right'
-				title='Create or update department'
-				withCloseButton={true}
-			>
-				<DepartmentForm
-					onRefetch={() => refetch()}
-					drawerHandler={drawerHandler}
-					action={action as 'CREATE' | 'EDIT'}
-					formData={
-						selectedDepartment as {
-							_id: string;
-							name: string;
-							note: string;
-						}
-					}
-				/>
-			</Drawer>
-		</div>
-	);
+        {loading && (
+          <>
+            {" "}
+            {new Array(12).fill(12).map((_, idx: number) => (
+              <Skeleton key={idx} h={250} radius={10} />
+            ))}
+          </>
+        )}
+      </div>
+
+      <Drawer
+        opened={openedDrawer}
+        onClose={drawerHandler.close}
+        position="right"
+        title="Create or update department"
+        withCloseButton={true}
+      >
+        <DepartmentForm
+          onFormSubmitted={handleOnRefetch}
+          drawerHandler={drawerHandler}
+          action={action as "CREATE" | "EDIT"}
+          formData={
+            selectedDepartment as {
+              _id: string;
+              name: string;
+              note: string;
+            }
+          }
+        />
+      </Drawer>
+    </div>
+  );
 };
 
 export default Departments;
