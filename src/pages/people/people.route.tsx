b346bc1a3d0f074f83320c3473c5878c77a403b8
MHRafix <fmhrafiz27@gmail.com>
2023-09-27 14:44:28 +0600

    navlink fixed

--- src/pages/people/people.route.tsx
+++ src/pages/people/people.route.tsx
@@ -1,44 +1,44 @@
-import { RouteObject } from "react-router-dom";
-import ClientPage from "./pages/client/client.page";
-import Suppliers from "./pages/suppliers/suppliers.page";
-import DashboardLayout from "@/_app/common/layouts/DashboardLayout";
-import { peopleNavlinks } from "./people.navlinks";
-import Departments from "./pages/employees/departments/departments.page";
-import Increments from "./pages/employees/increments/increments.page";
-import Employees from "./pages/employees/employees.page";
+import DashboardLayout from '@/_app/common/layouts/DashboardLayout';
+import { RouteObject } from 'react-router-dom';
+import ClientPage from './pages/client/client.page';
+import Employees from './pages/employees/all-employees.page';
+import Departments from './pages/employees/departments/departments.page';
+import Increments from './pages/employees/increments/increments.page';
+import Suppliers from './pages/suppliers/suppliers.page';
+import { peopleNavlinks } from './people.navlinks';
 
 export const peopleModuleRouter: RouteObject[] = [
-  {
-    path: "",
-    element: (
-      <DashboardLayout navlinks={peopleNavlinks} title="People" path="people" />
-    ),
-    children: [
-      {
-        path: "client",
-        element: <ClientPage />,
-      },
-      {
-        path: "suppliers",
-        element: <Suppliers />,
-      },
-      {
-        path: "employees",
-        children: [
-          {
-            path: "departments",
-            element: <Departments />,
-          },
-          {
-            path: "employees",
-            element: <Employees />,
-          },
-          {
-            path: "increments",
-            element: <Increments />,
-          },
-        ],
-      },
-    ],
-  },
+	{
+		path: '',
+		element: (
+			<DashboardLayout navlinks={peopleNavlinks} title='People' path='people' />
+		),
+		children: [
+			{
+				path: 'client',
+				element: <ClientPage />,
+			},
+			{
+				path: 'suppliers',
+				element: <Suppliers />,
+			},
+			{
+				path: 'employees',
+				children: [
+					{
+						path: 'departments',
+						element: <Departments />,
+					},
+					{
+						path: 'all-employees',
+						element: <Employees />,
+					},
+					{
+						path: 'increments',
+						element: <Increments />,
+					},
+				],
+			},
+		],
+	},
 ];
