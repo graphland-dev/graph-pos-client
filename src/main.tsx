ddb8a2e0ddc24fd5e2b8477ce4aab47bd48d7043
MHRafix <fmhrafiz27@gmail.com>
2023-09-28 17:43:14 +0600

    increment create drawer done

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import RootApp from "./RootApp";
-import "./styles/styles.scss";
-import { ApolloProvider } from "@apollo/client";
-import { apolloClient } from "./_app/clients/apollo.client";
+import { ApolloProvider } from '@apollo/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import RootApp from './RootApp';
+import { apolloClient } from './_app/clients/apollo.client';
+import './styles/styles.scss';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ApolloProvider client={apolloClient}>
-      <RootApp />
-    </ApolloProvider>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<React.StrictMode>
+		<ApolloProvider client={apolloClient}>
+			<RootApp />
+		</ApolloProvider>
+	</React.StrictMode>
 );
--- src/pages/people/pages/employees/increments/increments.page.tsx
+++ src/pages/people/pages/employees/increments/increments.page.tsx
@@ -1,5 +1,40 @@
+import { Button, Drawer, Flex, Space, Title } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { IconPlus } from '@tabler/icons-react';
+import { useState } from 'react';
+
 const Increments = () => {
-  return <div>Increments</div>;
+	const [openedDrawer, drawerHandler] = useDisclosure();
+	const [action, setAction] = useState<'CREATE' | 'EDIT'>();
+
+	return (
+		<div>
+			<Flex justify={'space-between'} align={'center'}>
+				<Title order={2}>Employee Increment</Title>
+				<Button
+					leftIcon={<IconPlus size={16} />}
+					onClick={() => {
+						drawerHandler.open();
+						setAction('CREATE');
+					}}
+				>
+					Add new
+				</Button>
+			</Flex>
+
+			<Space h={50} />
+
+			<Drawer
+				opened={openedDrawer}
+				onClose={drawerHandler.close}
+				position='right'
+				title='Create or update increment'
+				withCloseButton={true}
+			>
+				<div></div>
+			</Drawer>
+		</div>
+	);
 };
 
 export default Increments;
