a208af6a316b51dacb180fcc7ece1a0bfbf8aecc
KingRayhan <rayhan095@gmail.com>
2023-10-01 10:28:52 +0600

    accounting__removePayroll mutation

--- src/_app/configs/codegen.config.ts
+++ src/_app/configs/codegen.config.ts
@@ -1,15 +1,15 @@
-import { CodegenConfig } from '@graphql-codegen/cli';
+import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-	schema: `https://graph-erp-api.graphland.dev/graphql`,
-	// schema: `http://localhost:4001/graphql`,
-	documents: ['src/**/*.tsx'],
-	ignoreNoDocuments: true, // for better experience with the watcher
-	generates: {
-		'./src/_app/graphql-models/': {
-			preset: 'client',
-		},
-	},
+  schema: `https://graph-erp-api.graphland.dev/graphql`,
+  //   schema: `http://localhost:4001/graphql`,
+  documents: ["src/**/*.tsx"],
+  ignoreNoDocuments: true, // for better experience with the watcher
+  generates: {
+    "./src/_app/graphql-models/": {
+      preset: "client",
+    },
+  },
 };
 
 export default config;
