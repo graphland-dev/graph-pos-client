2ada839995a377a1b2daad80228f3f119a766a4b
KingRayhan <rayhan095@gmail.com>
2023-10-25 11:49:22 +0600

    refactore people modal

--- src/pages/people/pages/employees/employees/components/employee-details/Increments.tsx
+++ src/pages/people/pages/employees/employees/components/employee-details/Increments.tsx
@@ -1,86 +1,152 @@
+import { confirmModal } from "@/_app/common/confirm/confirm";
+import DataTable from "@/_app/common/data-table/DataTable";
+import {
+  EmployeeIncrement,
+  EmployeeIncrementsWithPagination,
+  MatchOperator,
+} from "@/_app/graphql-models/graphql";
+import { useMutation, useQuery } from "@apollo/client";
+import { Menu } from "@mantine/core";
+import { useSetState } from "@mantine/hooks";
+import { IconTrash } from "@tabler/icons-react";
+import { MRT_ColumnDef } from "mantine-react-table";
+import React, { useMemo } from "react";
+// import {
+//   INCREMENTS_QUERY,
+//   INCREMENT_DELETE_MUTATION,
+//   INCREMENT_EMPLOYEE_QUERY,
+// } from "./utils/increment.query";
+import dayjs from "dayjs";
+import {
+  INCREMENTS_QUERY,
+  INCREMENT_DELETE_MUTATION,
+} from "../../../increments/utils/increment.query";
 
-import { INCREMENTS_QUERY } from '../../../increments/utils/increment.query';
-import { EmployeeIncrementsWithPagination } from '@/_app/graphql-models/graphql';
-import { useQuery } from '@apollo/client';
-import { Button, Divider, Paper, Text, Title } from '@mantine/core';
-import { IconChartArrowsVertical } from '@tabler/icons-react';
-import dayjs from 'dayjs';
+interface IState {
+  refetching: boolean;
+}
 
 interface IIncrementsDetailsProps {
   id: string | undefined;
 }
 
+const EmployeeIncrementsPage: React.FC<IIncrementsDetailsProps> = ({ id }) => {
+  // const [openedDrawer, drawerHandler] = useDisclosure();
+  const [state, setState] = useSetState<IState>({
+    refetching: false,
+  });
 
-const Increments: React.FC<IIncrementsDetailsProps> = ({id}) => {
+  const {
+    data: increments,
+    loading: fetchingIncrements,
+    refetch,
+  } = useQuery<{
+    people__employeeIncrements: EmployeeIncrementsWithPagination;
+  }>(INCREMENTS_QUERY);
 
-    const { data: increments } = useQuery<{
-      people__employeeIncrements: EmployeeIncrementsWithPagination;
-    }>(INCREMENTS_QUERY, {
-      variables: {
-        where: {
-          filters: [
-            {
-              key: "employee",
-              operator: "eq",
-              value: id,
-            },
-          ],
-        },
+  const [deleteIncrementMutation] = useMutation(INCREMENT_DELETE_MUTATION, {
+    onCompleted: () => handleRefetch({}),
+  });
+
+  const handleRefetch = (variables: any) => {
+    setState({ refetching: true });
+    refetch(variables).finally(() => {
+      setState({ refetching: false });
+    });
+  };
+
+  const handleDeleteIncrement = (_id: string) => {
+    confirmModal({
+      title: "Sure to delete account?",
+      description: "Be careful!! Once you deleted, it can not be undone",
+      isDangerous: true,
+      onConfirm() {
+        deleteIncrementMutation({
+          variables: {
+            where: { key: "_id", operator: MatchOperator.Eq, value: _id },
+          },
+        });
       },
     });
-    
-    const incrementsData = increments?.people__employeeIncrements.nodes ?? []
-    return (
-      <div>
-        <Paper shadow="md" p={"lg"}>
-          <div className="flex  items-center gap-4 justify-between">
-            <div className="flex  items-center gap-4">
-              <IconChartArrowsVertical size={24} />
-              <Title order={3}>Increment Information</Title>
-            </div>
-            <Button>Add Info</Button>
-          </div>
-          <Divider my="sm" />
-          <div className="flex flex-col gap-3 w-8/12">
-            {incrementsData.map((increment) => (
-              <>
-                <div className="flex justify-between ">
-                  <Title color="gray" order={4}>
-                    Name:
-                  </Title>
-                  <Text>{increment?.employee?.name}</Text>
-                </div>
+  };
 
-                <div className="flex justify-between ">
-                  <Title color="gray" order={4}>
-                    Note:
-                  </Title>
-                  <Text>{increment.note}</Text>
-                </div>
-                <div className="flex justify-between">
-                  <Title color="gray" order={4}>
-                    Increment Date:
-                  </Title>
-                  <Text>
-                    {dayjs(increment.date).format("MMMM D, YYYY h:mm A")}
-                  </Text>
-                </div>
-                <div className="flex justify-between">
-                  <Title color="gray" order={4}>
-                    Create date:
-                  </Title>
-                  <Text>
-                    {dayjs(increment.createdAt).format("MMMM D, YYYY h:mm A")}
-                  </Text>
-                </div>
-              </>
-            ))}
-          </div>
-        </Paper>
-        
-        {/* <pre>{JSON.stringify(incrementsData, null, 2)}</pre> */}
-      </div>
-    );
-}
+  const columns = useMemo<MRT_ColumnDef<any>[]>(
+    () => [
+      {
+        accessorKey: "amount",
+        header: "Increment Amount",
+      },
+      {
+        accessorKey: "note",
+        header: "Note",
+      },
+      {
+        accessorFn: (row: EmployeeIncrement) =>
+          dayjs(row?.date).format("MMMM D, YYYY h:mm A"),
+        accessorKey: "date",
+        header: "Date",
+      },
+    ],
+    []
+  );
 
-export default Increments
+  return (
+    <div>
+      <DataTable
+        columns={columns}
+        data={increments?.people__employeeIncrements.nodes ?? []}
+        refetch={handleRefetch}
+        totalCount={
+          increments?.people__employeeIncrements.meta?.totalCount ?? 100
+        }
+        filters={[
+          {
+            key: "employee",
+            operator: MatchOperator.Eq,
+            value: id,
+          },
+        ]}
+        RowActionMenu={(row: EmployeeIncrement) => (
+          <>
+            <Menu.Item
+              onClick={() => handleDeleteIncrement(row._id)}
+              icon={<IconTrash size={18} />}
+            >
+              Delete
+            </Menu.Item>
+          </>
+        )}
+        ActionArea={
+          <>
+            {/* <Button
+              leftIcon={<IconPlus size={16} />}
+              onClick={drawerHandler.open}
+              size="sm"
+            >
+              Add new
+            </Button> */}
+          </>
+        }
+        loading={fetchingIncrements || state.refetching}
+      />
+
+      {/* <Drawer
+        opened={openedDrawer}
+        onClose={drawerHandler.close}
+        position="right"
+        title="Create increment"
+        withCloseButton={true}
+      >
+        <IncrementForm
+          employees={data?.people__employees?.nodes as Employee[]}
+          onFormSubmitted={() => {
+            refetch();
+            drawerHandler.close();
+          }}
+        />
+      </Drawer> */}
+    </div>
+  );
+};
+
+export default EmployeeIncrementsPage;
