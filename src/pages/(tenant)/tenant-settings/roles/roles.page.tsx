dbd6bd6b98c512a8f93b79f1842ab6d2f2a7b3c4
kingrayhan <rayhan095@gmail.com>
2024-02-05 11:52:45 +0600

    Refactor query.gql.ts to use double quotes for import statement

--- src/pages/(tenant)/tenant-settings/roles/roles.page.tsx
+++ src/pages/(tenant)/tenant-settings/roles/roles.page.tsx
@@ -1,304 +1,83 @@
-import { Role } from '@/_app/graphql-models/graphql';
-import { useQuery } from '@apollo/client';
+import { Role } from "@/_app/graphql-models/graphql";
+import { useQuery } from "@apollo/client";
 import {
-	Accordion,
-	Button,
-	Checkbox,
-	Flex,
-	Group,
-	Skeleton,
-	Space,
-	Title,
-} from '@mantine/core';
-import { useEffect } from 'react';
-import { useFieldArray, useForm } from 'react-hook-form';
-import { CURRENT__TENANT__ROLES } from './utils/query.gql';
-
-const appCollectionPermissions = [
-	{
-		name: 'Accounting',
-		collections: [
-			'accounting__Account',
-			'accounting__Expense',
-			'accounting__ExpenseCategory',
-			'accounting__Payroll',
-			'accounting__Transaction',
-			'accounting__Transfer',
-		],
-	},
-	{
-		name: 'People',
-		collections: [
-			'people__Client',
-			'people__Employee',
-			'people__EmployeeDepartment',
-			'people__EmployeeIncrement',
-			'people__Supplier',
-		],
-	},
-	{
-		name: 'Inventory',
-		collections: [
-			'inventory__Product',
-			'inventory__ProductCategory',
-			'inventory__ProductPurchase',
-			'inventory__ProductStock',
-		],
-	},
-	{
-		name: 'Setup',
-		collections: ['setup__Brand', 'setup__Unit', 'setup__Vat'],
-	},
-];
-
-interface IFormPayload {
-	roles: {
-		name: string;
-		permissions: {
-			collection: string;
-			actions: string[];
-		}[];
-	}[];
-}
+  Accordion,
+  Badge,
+  Button,
+  Flex,
+  Group,
+  Skeleton,
+  Space,
+  Text,
+  Title,
+} from "@mantine/core";
+import RolePermissionForm from "./components/RolePermissionForm";
+import { CURRENT__TENANT__ROLES } from "./utils/query.gql";
 
 const RolesPage = () => {
-	const { data, loading } = useQuery(CURRENT__TENANT__ROLES);
+  const { data, loading } = useQuery<{ identity__currentTenantRoles: Role[] }>(
+    CURRENT__TENANT__ROLES
+  );
 
-	const { control, setValue, watch, handleSubmit } = useForm<IFormPayload>({
-		defaultValues: {
-			roles: [],
-		},
-	});
+  return (
+    <div>
+      <Flex>
+        <Title order={2} fw={700}>
+          Roles Management
+        </Title>
+      </Flex>
 
-	const { fields } = useFieldArray({
-		name: 'roles',
-		control,
-	});
+      <Space h={"lg"} />
 
-	useEffect(() => {
-		setValue(
-			'roles',
-			data?.identity__currentTenantRoles?.map((role: Role) => ({
-				name: role.name,
-				permissions: role.permissions,
-			}))
-		);
-	}, [data?.identity__currentTenantRoles]);
+      {loading && (
+        <>
+          {new Array(12).fill(12).map(() => (
+            <Skeleton h={80} radius={5} my={10} />
+          ))}
+        </>
+      )}
 
-	const handlePermissionChange = (
-		roleIndex: number,
-		collection: string,
-		checked: boolean,
-		actionName: string
-	) => {
-		const rolePermissions = watch(`roles.${roleIndex}.permissions`);
-		const collectionIndex = rolePermissions.findIndex(
-			(permission) => permission.collection === collection
-		);
-		if (checked) {
-			setValue(
-				`roles.${roleIndex}.permissions`,
-				rolePermissions.map((permission, index) => {
-					if (index === collectionIndex) {
-						return {
-							...permission,
-							actions: [...permission.actions, actionName],
-						};
-					}
-					return permission;
-				})
-			);
-		} else {
-			setValue(
-				`roles.${roleIndex}.permissions`,
-				rolePermissions.map((permission, index) => {
-					if (index === collectionIndex) {
-						return {
-							...permission,
-							actions: [],
-						};
-					}
-					return permission;
-				})
-			);
-		}
-	};
+      <Accordion defaultValue="customization" variant="contained">
+        {data?.identity__currentTenantRoles?.map((role) => (
+          <Accordion.Item value={role?._id} key={role?._id}>
+            <Accordion.Control>
+              <Flex justify={"space-between"} align={"center"}>
+                <Text fz={18}>
+                  {role?.name}
+                  {!role?.tenant && (
+                    <Badge size="xs" color="red.5">
+                      Readonly
+                    </Badge>
+                  )}
+                </Text>
+                <Group position="right">
+                  <Button variant="subtle" color="teal" size="xs">
+                    Clone
+                  </Button>
+                  {role?.tenant && (
+                    <Button variant="light" color="red" size="xs">
+                      Delete
+                    </Button>
+                  )}
+                </Group>
+              </Flex>
+            </Accordion.Control>
+            <Space h={"xs"} />
+            <Accordion.Panel>
+              <RolePermissionForm
+                rolePermissions={role.permissions || []}
+                isReadOnly={Boolean(!role.tenant)}
+              />
+            </Accordion.Panel>
+          </Accordion.Item>
+        ))}
+      </Accordion>
 
-	// const [] = useMutation();
+      <Space h={"md"} />
 
-	const onSubmitForm = (values: any) => {
-		console.log(values);
-	};
-
-	// console.log(watch('roles'));
-	return (
-		<div>
-			<Flex>
-				<Title order={2} fw={700}>
-					Roles Management
-				</Title>
-			</Flex>
-
-			<Space h={'lg'} />
-
-			{loading && (
-				<>
-					{new Array(12).fill(12).map(() => (
-						<Skeleton h={80} radius={5} my={10} />
-					))}
-				</>
-			)}
-
-			<form onSubmit={handleSubmit(onSubmitForm)}>
-				<Accordion defaultValue='customization' variant='contained'>
-					{fields.map((role, roleIndex) => (
-						<Accordion.Item value={role?.id} key={roleIndex}>
-							<Accordion.Control>
-								<Flex justify={'space-between'} align={'center'}>
-									<Title order={4}>{role?.name} Module</Title>
-									<Group position='right'>
-										<Button variant='subtle' color='teal'>
-											Clone
-										</Button>
-										<Button variant='light' color='red'>
-											Delete
-										</Button>
-									</Group>
-								</Flex>
-							</Accordion.Control>
-							<Space h={'xs'} />
-							<Accordion.Panel>
-								<div>
-									{appCollectionPermissions.map((collection) => (
-										<div>
-											<Title order={5}>{collection.name}</Title>
-
-											<Space h={'xs'} />
-
-											{collection.collections.map((collectionName) => (
-												<div>
-													{/* {collectionName} */}
-													<Flex gap={10} my={10}>
-														<Checkbox
-															label='*'
-															value={'*'}
-															onChange={(e) => {
-																handlePermissionChange(
-																	roleIndex,
-																	collectionName,
-																	e.target.checked,
-																	'*'
-																);
-															}}
-															color='teal'
-															radius={0}
-														/>
-														<Checkbox
-															label='Read'
-															value={'read'}
-															checked={watch(`roles.${roleIndex}.permissions`)
-																.find(
-																	(permission) =>
-																		permission.collection === collectionName
-																)
-																?.actions.includes('read')}
-															onChange={(e) => {
-																handlePermissionChange(
-																	roleIndex,
-																	collectionName,
-																	e.target.checked,
-																	'read'
-																);
-															}}
-															color='teal'
-															radius={0}
-														/>
-														<Checkbox
-															label='Create'
-															value={'create'}
-															checked={watch(`roles.${roleIndex}.permissions`)
-																.find(
-																	(permission) =>
-																		permission.collection === collectionName
-																)
-																?.actions.includes('create')}
-															onChange={(e) => {
-																handlePermissionChange(
-																	roleIndex,
-																	collectionName,
-																	e.target.checked,
-																	'create'
-																);
-															}}
-															color='teal'
-															radius={0}
-														/>
-														<Checkbox
-															label='Update'
-															value={'update'}
-															checked={
-																watch(`roles.${roleIndex}.permissions`)
-																	.find(
-																		(permission) =>
-																			permission.collection === collectionName
-																	)
-																	?.actions.includes('update') ||
-																watch(`roles.${roleIndex}.permissions`)
-																	.find(
-																		(permission) =>
-																			permission.collection === collectionName
-																	)
-																	?.actions.includes('*')
-															}
-															onChange={(e) => {
-																handlePermissionChange(
-																	roleIndex,
-																	collectionName,
-																	e.target.checked,
-																	'update'
-																);
-															}}
-															color='teal'
-															radius={0}
-														/>
-
-														<Checkbox
-															label='Delete'
-															value={'delete'}
-															checked={watch(`roles.${roleIndex}.permissions`)
-																.find(
-																	(permission) =>
-																		permission.collection === collectionName
-																)
-																?.actions.includes('delete')}
-															onChange={(e) => {
-																handlePermissionChange(
-																	roleIndex,
-																	collectionName,
-																	e.target.checked,
-																	'delete'
-																);
-															}}
-															color='teal'
-															radius={0}
-														/>
-													</Flex>
-												</div>
-											))}
-										</div>
-									))}
-								</div>
-							</Accordion.Panel>
-						</Accordion.Item>
-					))}
-				</Accordion>
-
-				<Space h={'md'} />
-
-				<Button type='submit'>Save</Button>
-			</form>
-		</div>
-	);
+      <Button type="submit">Save</Button>
+    </div>
+  );
 };
 
 export default RolesPage;
--- src/pages/(tenant)/tenant-settings/roles/utils/query.gql.ts
+++ src/pages/(tenant)/tenant-settings/roles/utils/query.gql.ts
@@ -1,15 +1,15 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const CURRENT__TENANT__ROLES = gql`
-	query {
-		identity__currentTenantRoles {
-			_id
-			name
-			permissions {
-				collectionName
-				actions
-			}
-			tenant
-		}
-	}
+  query {
+    identity__currentTenantRoles {
+      _id
+      name
+      permissions {
+        collectionName
+        actions
+      }
+      tenant
+    }
+  }
 `;
