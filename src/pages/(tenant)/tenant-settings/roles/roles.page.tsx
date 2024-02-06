f93816548a7d80b1130682e9ebf2a779c71f541d
MHRafix <fmhrafiz27@gmail.com>
2024-02-06 12:12:20 +0600

    roles and organization employee done

--- src/pages/(tenant)/tenant-settings/roles/roles.page.tsx
+++ src/pages/(tenant)/tenant-settings/roles/roles.page.tsx
@@ -1,30 +1,98 @@
+import { Notify } from '@/_app/common/Notification/Notify';
 import { Role } from '@/_app/graphql-models/graphql';
-import { useQuery } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
+import { ErrorMessage } from '@hookform/error-message';
+import { yupResolver } from '@hookform/resolvers/yup';
 import {
 	Accordion,
 	Badge,
 	Button,
 	Flex,
 	Group,
+	Input,
+	Modal,
 	Skeleton,
 	Space,
 	Text,
 	Title,
 } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { IconPlus } from '@tabler/icons-react';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import * as Yup from 'yup';
 import RolePermissionForm from './components/RolePermissionForm';
-import { CURRENT__TENANT__ROLES } from './utils/query.gql';
+import {
+	CREATE_OR_CLONE_ROLE,
+	CURRENT__TENANT__ROLES,
+} from './utils/query.gql';
 //
 const RolesPage = () => {
-	const { data, loading } = useQuery<{ identity__currentTenantRoles: Role[] }>(
-		CURRENT__TENANT__ROLES
+	const [opened, handler] = useDisclosure();
+	const [selectedRole, setSelectedRole] = useState<Role | null>();
+
+	const { data, loading, refetch } = useQuery<{
+		identity__currentTenantRoles: Role[];
+	}>(CURRENT__TENANT__ROLES);
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		reset,
+	} = useForm<{
+		name: string;
+	}>({
+		resolver: yupResolver(
+			Yup.object().shape({
+				name: Yup.string().required().label('Role name'),
+			})
+		),
+	});
+
+	const [cloneRole, { loading: cloningOrCreating }] = useMutation(
+		CREATE_OR_CLONE_ROLE,
+		Notify({
+			sucTitle: 'New role created successfully.',
+			onSuccess() {
+				refetch();
+				handler.close();
+				reset({
+					name: '',
+				});
+				setSelectedRole(null);
+			},
+		})
 	);
+
+	const onSubmitCloneForm = (values: { name: string }) => {
+		cloneRole({
+			variables: {
+				body: {
+					name: values?.name,
+					permissions: selectedRole?.permissions,
+					tenantId: selectedRole?.tenant,
+				},
+			},
+		});
+	};
 
 	return (
 		<div>
-			<Flex>
+			{/* {JSON.stringify(errors, null)} */}
+			<Flex justify={'space-between'} align={'center'}>
 				<Title order={2} fw={700}>
 					Roles Management
 				</Title>
+				<Button
+					size='xs'
+					onClick={() => {
+						handler.open();
+					}}
+					leftIcon={<IconPlus />}
+				>
+					Add new
+				</Button>{' '}
 			</Flex>
 
 			<Space h={'lg'} />
@@ -51,7 +119,25 @@ const RolesPage = () => {
 									)}
 								</Text>
 								<Group position='right'>
-									<Button variant='subtle' color='teal' size='xs'>
+									<Button
+										variant='subtle'
+										color='teal'
+										size='xs'
+										onClick={() => {
+											setSelectedRole({
+												createdAt: role?.createdAt,
+												updatedAt: role?.updatedAt,
+												_id: role?._id,
+												name: role?.name,
+												permissions: role?.permissions?.map((permission) => ({
+													collectionName: permission.collectionName,
+													actions: permission.actions,
+												})),
+												tenant: role?.tenant,
+											});
+											handler.open();
+										}}
+									>
 										Clone
 									</Button>
 									{role?.tenant && (
@@ -63,6 +149,25 @@ const RolesPage = () => {
 							</Flex>
 						</Accordion.Control>
 						<Space h={'xs'} />
+						<Modal
+							opened={opened}
+							onClose={handler.close}
+							title={'Clone Role'}
+							centered
+						>
+							<form onSubmit={handleSubmit(onSubmitCloneForm)}>
+								<Input.Wrapper
+									label={'Role name'}
+									error={<ErrorMessage errors={errors} name='name' />}
+								>
+									<Input placeholder='Type name' {...register('name')} />
+								</Input.Wrapper>
+								<Space h={'sm'} />
+								<Button type='submit' loading={cloningOrCreating}>
+									Save
+								</Button>
+							</form>
+						</Modal>
 						<Accordion.Panel>
 							<RolePermissionForm
 								rolePermissions={role.permissions || []}
--- src/pages/(tenant)/tenant-settings/roles/utils/query.gql.ts
+++ src/pages/(tenant)/tenant-settings/roles/utils/query.gql.ts
@@ -1,5 +1,13 @@
 import { gql } from '@apollo/client';
 
+export const CREATE_OR_CLONE_ROLE = gql`
+	mutation Identity__createRole($body: CreateRoleInput!) {
+		identity__createRole(body: $body) {
+			_id
+		}
+	}
+`;
+
 export const CURRENT__TENANT__ROLES = gql`
 	query {
 		identity__currentTenantRoles {
