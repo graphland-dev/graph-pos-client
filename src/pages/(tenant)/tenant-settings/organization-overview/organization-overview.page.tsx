3950a4f40294831e04cdf1a1efcc518bb73efb35
NishatFariza <nishatfariza012@gmail.com>
2024-02-17 13:51:49 +0600

    Profile settings avatar uploader

--- src/pages/(tenant)/tenant-settings/organization-overview/organization-overview.page.tsx
+++ src/pages/(tenant)/tenant-settings/organization-overview/organization-overview.page.tsx
@@ -1,4 +1,8 @@
 import { Notify } from "@/_app/common/Notification/Notify";
+import { getFileUrl } from "@/_app/common/utils/getFileUrl";
+import { ServerFileReference } from "@/_app/graphql-models/graphql";
+import { useServerFile } from "@/_app/hooks/use-upload-file";
+import { FOLDER__NAME } from "@/_app/models/FolderName";
 import { userTenantsAtom } from "@/_app/states/user.atom";
 import { useMutation } from "@apollo/client";
 import { ErrorMessage } from "@hookform/error-message";
@@ -6,19 +10,22 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import {
   Button,
   Flex,
+  Image,
   Input,
   Paper,
   Space,
   Text,
   Textarea,
   Title,
+  clsx,
   rem,
 } from "@mantine/core";
-import { Dropzone, IMAGE_MIME_TYPE } from "@mantine/dropzone";
-import { IconUpload } from "@tabler/icons-react";
+import { Dropzone } from "@mantine/dropzone";
+import { IconPhoto } from "@tabler/icons-react";
 import { useAtom } from "jotai";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import { FaCamera } from "react-icons/fa";
 import { useParams } from "react-router-dom";
 import {
   IOrganizationFormType,
@@ -26,10 +33,27 @@ import {
 } from "../utils/form.validation";
 import { ORGANIZATION_OVERVIEW_INFO_UPDATE_MUTATION } from "../utils/overview.query.gql";
 
-const OrganizationOverviewPage = () => {
+interface IProp {
+  onChange: (file: ServerFileReference) => void;
+  file?: ServerFileReference;
+  folder?: FOLDER__NAME;
+  isLogo?: boolean;
+}
+
+const OrganizationOverviewPage: React.FC<IProp> = ({
+  file,
+  folder = FOLDER__NAME.USER__AVATAR,
+  isLogo = false,
+}) => {
   const params = useParams<{ tenant: string }>();
   const [tenants] = useAtom(userTenantsAtom);
   const tenant = tenants?.find((t) => t.uid === params.tenant);
+
+
+
+  const { uploadFile, uploading } = useServerFile();
+  const [organizationLogo, setOrganizationLogo] = useState({})
+  console.log(organizationLogo);
 
   // form initiated
   const {
@@ -60,14 +84,12 @@ const OrganizationOverviewPage = () => {
   // submit form with update mutation
   const onSubmit = (values: IOrganizationFormType) => {
     updateOrganizationInfo({
-      variables: { input: values },
+      variables: { input: values},
     });
   };
 
   return (
     <div>
-      {/* <pre>{JSON.stringify(tenant, null, 2)}</pre> */}
-
       <Paper px={20} py={20} radius={10} className="lg:w-8/12">
         <Title order={3}>Organization overview</Title>
 
@@ -76,24 +98,118 @@ const OrganizationOverviewPage = () => {
         <form onSubmit={handleSubmit(onSubmit)}>
           <Flex align={"center"} gap={20}>
             <Dropzone
-              onDrop={(files) => console.log("accepted files", files)}
-              onReject={(files) => console.log("rejected files", files)}
-              maxSize={5 * 1024 ** 2}
-              accept={IMAGE_MIME_TYPE}
-              w={100}
-              h={100}
-              disabled
-              radius={100}
-              className="flex items-center justify-center"
+              onDrop={async (files) => {
+                await uploadFile({
+                  files,
+                  folder,
+                }).then((res) => {
+                  res?.data?.length > 0 && setOrganizationLogo(res.data[0]);
+                });
+                // result === true && onChange(files[0]);
+              }}
+              loading={uploading}
+              maxSize={3 * 1024 ** 2}
+              className={clsx(
+                "flex items-center justify-center group p-0 m-0 h-[200px] w-[200px] rounded-full",
+                {
+                  " h-[100px] w-[100px] ": isLogo,
+                }
+              )}
             >
-              <IconUpload
-                style={{
-                  width: rem(52),
-                  height: rem(52),
-                }}
-                stroke={1.5}
-              />
+              {!file?.path ? (
+                <IconPhoto
+                  style={{
+                    width: rem(isLogo ? 33 : 52),
+                    height: rem(isLogo ? 33 : 52),
+                    color: "var(--mantine-color-blue-6)",
+                  }}
+                  stroke={1.5}
+                />
+              ) : (
+                <div className="relative">
+                  <Image
+                    className={clsx(
+                      "w-[200px] h-[200px] rounded-full object-cover",
+                      {
+                        " h-[100px] w-[100px] ": isLogo,
+                      }
+                    )}
+                    src={getFileUrl(file)}
+                  />
+
+                  <FaCamera
+                    size={isLogo ? 33 : 55}
+                    color="white"
+                    className="absolute shadow-xl opacity-0 group-hover:opacity-100"
+                    style={{
+                      transform: "translate(-50%, -50%)",
+                      top: "50%",
+                      left: "50%",
+                      borderRadius: "5px",
+                      fontSize: "12px",
+                      fontWeight: "bold",
+                      transition: "all 0.5s ease-in-out",
+                    }}
+                  />
+                </div>
+              )}
             </Dropzone>
+            {/* 
+            <Dropzone
+              onDrop={async (files) => {
+                const result = await useServerFile({
+                  file: files[0],
+                  folder,
+                });
+                onChange(result.data);
+              }}
+              loading={uploading}
+              maxSize={3 * 1024 ** 2}
+              className={clsx(
+                "flex items-center justify-center group p-0 m-0 h-[200px] w-[200px] rounded-full",
+                {
+                  " h-[100px] w-[100px] ": isLogo,
+                }
+              )}
+            >
+              {!file?.path ? (
+                <IconPhoto
+                  style={{
+                    width: rem(isLogo ? 33 : 52),
+                    height: rem(isLogo ? 33 : 52),
+                    color: "var(--mantine-color-blue-6)",
+                  }}
+                  stroke={1.5}
+                />
+              ) : (
+                <div className="relative">
+                  <Image
+                    className={clsx(
+                      "w-[200px] h-[200px] rounded-full object-cover",
+                      {
+                        " h-[100px] w-[100px] ": isLogo,
+                      }
+                    )}
+                    src={getFileUrl(file)}
+                  />
+
+                  <FaCamera
+                    size={isLogo ? 33 : 55}
+                    color="white"
+                    className="absolute shadow-xl opacity-0 group-hover:opacity-100"
+                    style={{
+                      transform: "translate(-50%, -50%)",
+                      top: "50%",
+                      left: "50%",
+                      borderRadius: "5px",
+                      fontSize: "12px",
+                      fontWeight: "bold",
+                      transition: "all 0.5s ease-in-out",
+                    }}
+                  />
+                </div>
+              )}
+            </Dropzone> */}
 
             <Text fw={500}>Organization Logo</Text>
           </Flex>
