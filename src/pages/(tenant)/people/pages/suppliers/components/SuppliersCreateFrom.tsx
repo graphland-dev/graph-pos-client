5ee757a45c66919b7f375b94e451b463fca5f3f4
NishatFariza <nishatfariza012@gmail.com>
2024-02-17 10:56:48 +0600

    supplier page attatchments done

--- src/pages/(tenant)/people/pages/suppliers/components/SuppliersCreateFrom.tsx
+++ src/pages/(tenant)/people/pages/suppliers/components/SuppliersCreateFrom.tsx
@@ -1,185 +1,185 @@
-import { Notify } from '@/_app/common/Notification/Notify';
-import { MatchOperator, Supplier } from '@/_app/graphql-models/graphql';
-import { useMutation } from '@apollo/client';
-import { ErrorMessage } from '@hookform/error-message';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { Button, Input, Space, Textarea } from '@mantine/core';
-import React, { useEffect } from 'react';
-import { useForm } from 'react-hook-form';
-import * as Yup from 'yup';
+import { Notify } from "@/_app/common/Notification/Notify";
+import { MatchOperator, Supplier } from "@/_app/graphql-models/graphql";
+import { useMutation } from "@apollo/client";
+import { ErrorMessage } from "@hookform/error-message";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Button, Input, Space, Textarea } from "@mantine/core";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import * as Yup from "yup";
 import {
-	PEOPLE_CREATE_SUPPLIERS,
-	PEOPLE_UPDATE_SUPPLIERS,
-} from '../utils/suppliers.query';
+  PEOPLE_CREATE_SUPPLIERS,
+  PEOPLE_UPDATE_SUPPLIERS,
+} from "../utils/suppliers.query";
 
 interface IClientFormProps {
-	onFormSubmitted: () => void;
+  onFormSubmitted: () => void;
 
-	formData?: Supplier;
+  formData?: Supplier;
 
-	action: 'CREATE' | 'EDIT';
+  action: "CREATE" | "EDIT";
 }
 
 const SuppliersCreateFrom: React.FC<IClientFormProps> = ({
-	onFormSubmitted,
-	action,
-	formData,
+  onFormSubmitted,
+  action,
+  formData,
 }) => {
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-		setValue,
-		reset,
-	} = useForm({
-		defaultValues: {
-			name: '',
-			companyName: '',
-			contactNumber: '',
-			email: '',
-			address: '',
-		},
-		resolver: yupResolver(formValidationSchema),
-	});
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    reset,
+  } = useForm({
+    defaultValues: {
+      name: "",
+      companyName: "",
+      contactNumber: "",
+      email: "",
+      address: "",
+    },
+    resolver: yupResolver(formValidationSchema),
+  });
 
-	useEffect(() => {
-		setValue('name', formData?.name as string);
-		setValue('companyName', formData?.companyName as string);
-		setValue('email', formData?.email as string);
-		setValue('address', formData?.address as string);
-		setValue('contactNumber', formData?.contactNumber as string);
-	}, [formData]);
+  useEffect(() => {
+    setValue("name", formData?.name as string);
+    setValue("companyName", formData?.companyName as string);
+    setValue("email", formData?.email as string);
+    setValue("address", formData?.address as string);
+    setValue("contactNumber", formData?.contactNumber as string);
+  }, [formData]);
 
-	const [createSupplier, { loading: creating }] = useMutation(
-		PEOPLE_CREATE_SUPPLIERS,
-		Notify({
-			sucTitle: 'Supplier successfully created!',
-			onSuccess() {
-				reset();
-				onFormSubmitted();
-			},
-		})
-	);
+  const [createSupplier, { loading: creating }] = useMutation(
+    PEOPLE_CREATE_SUPPLIERS,
+    Notify({
+      sucTitle: "Supplier successfully created!",
+      onSuccess() {
+        reset();
+        onFormSubmitted();
+      },
+    })
+  );
 
-	const [updateSupplier, { loading: updating }] = useMutation(
-		PEOPLE_UPDATE_SUPPLIERS,
-		Notify({
-			sucTitle: 'Supplier successfully updated!',
-			onSuccess() {
-				reset();
-				onFormSubmitted();
-			},
-		})
-	);
+  const [updateSupplier, { loading: updating }] = useMutation(
+    PEOPLE_UPDATE_SUPPLIERS,
+    Notify({
+      sucTitle: "Supplier successfully updated!",
+      onSuccess() {
+        reset();
+        onFormSubmitted();
+      },
+    })
+  );
 
-	const onSubmit = (values: ISUPPLIERSCREATEFORM) => {
-		if (action === 'CREATE') {
-			createSupplier({
-				variables: {
-					body: {
-						name: values.name,
-						companyName: values.companyName,
-						email: values.email,
-						contactNumber: values.contactNumber,
-						address: values.address,
-					},
-				},
-			});
-		} else {
-			updateSupplier({
-				variables: {
-					where: {
-						key: '_id',
-						operator: MatchOperator.Eq,
-						value: formData?._id,
-					},
+  const onSubmit = (values: ISUPPLIERSCREATEFORM) => {
+    if (action === "CREATE") {
+      createSupplier({
+        variables: {
+          body: {
+            name: values.name,
+            companyName: values.companyName,
+            email: values.email,
+            contactNumber: values.contactNumber,
+            address: values.address,
+          },
+        },
+      });
+    } else {
+      updateSupplier({
+        variables: {
+          where: {
+            key: "_id",
+            operator: MatchOperator.Eq,
+            value: formData?._id,
+          },
 
-					body: {
-						name: values.name,
-						companyName: values.companyName,
-						email: values.email,
-						contactNumber: values.contactNumber,
-						address: values.address,
-					},
-				},
-			});
-		}
-	};
-	return (
-		<div>
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<Input.Wrapper
-					label='Name'
-					error={<ErrorMessage name='name' errors={errors} />}
-				>
-					<Input placeholder='Write client name' {...register('name')} />
-				</Input.Wrapper>
+          body: {
+            name: values.name,
+            companyName: values.companyName,
+            email: values.email,
+            contactNumber: values.contactNumber,
+            address: values.address,
+          },
+        },
+      });
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input.Wrapper
+          label="Name"
+          error={<ErrorMessage name="name" errors={errors} />}
+        >
+          <Input placeholder="Write client name" {...register("name")} />
+        </Input.Wrapper>
 
-				<Space h={'sm'} />
+        <Space h={"sm"} />
 
-				<Input.Wrapper
-					label='Company name'
-					error={<ErrorMessage name='companyName' errors={errors} />}
-				>
-					<Input
-						placeholder='Write company name'
-						{...register('companyName')}
-					/>
-				</Input.Wrapper>
+        <Input.Wrapper
+          label="Company name"
+          error={<ErrorMessage name="companyName" errors={errors} />}
+        >
+          <Input
+            placeholder="Write company name"
+            {...register("companyName")}
+          />
+        </Input.Wrapper>
 
-				<Space h={'sm'} />
+        <Space h={"sm"} />
 
-				<Input.Wrapper
-					label='Email'
-					error={<ErrorMessage name='email' errors={errors} />}
-				>
-					<Input placeholder='Write email' {...register('email')} />
-				</Input.Wrapper>
+        <Input.Wrapper
+          label="Email"
+          error={<ErrorMessage name="email" errors={errors} />}
+        >
+          <Input placeholder="Write email" {...register("email")} />
+        </Input.Wrapper>
 
-				<Space h={'sm'} />
+        <Space h={"sm"} />
 
-				<Input.Wrapper
-					label='Contact number'
-					error={<ErrorMessage name='contactNumber' errors={errors} />}
-				>
-					<Input
-						placeholder='Write contact number'
-						{...register('contactNumber')}
-					/>
-				</Input.Wrapper>
-				<Space h={'sm'} />
+        <Input.Wrapper
+          label="Contact number"
+          error={<ErrorMessage name="contactNumber" errors={errors} />}
+        >
+          <Input
+            placeholder="Write contact number"
+            {...register("contactNumber")}
+          />
+        </Input.Wrapper>
+        <Space h={"sm"} />
 
-				<Input.Wrapper
-					label='Address'
-					error={<ErrorMessage name='address' errors={errors} />}
-				>
-					<Textarea placeholder='Write address' {...register('address')} />
-				</Input.Wrapper>
+        <Input.Wrapper
+          label="Address"
+          error={<ErrorMessage name="address" errors={errors} />}
+        >
+          <Textarea placeholder="Write address" {...register("address")} />
+        </Input.Wrapper>
 
-				<Space h={'sm'} />
+        <Space h={"sm"} />
 
-				<Button type='submit' loading={creating || updating} fullWidth>
-					Save
-				</Button>
-			</form>
-		</div>
-	);
+        <Button type="submit" loading={creating || updating} fullWidth>
+          Save
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default SuppliersCreateFrom;
 
 export const formValidationSchema = Yup.object().shape({
-	name: Yup.string().required().label('Name'),
-	companyName: Yup.string().required().label('Company Name'),
-	contactNumber: Yup.string().required().label('Contact number'),
-	email: Yup.string().email().required().label('Email'),
-	address: Yup.string().required().label('Address'),
+  name: Yup.string().required().label("Name"),
+  companyName: Yup.string().required().label("Company Name"),
+  contactNumber: Yup.string().required().label("Contact number"),
+  email: Yup.string().email().required().label("Email"),
+  address: Yup.string().required().label("Address"),
 });
 
 interface ISUPPLIERSCREATEFORM {
-	name: string;
-	companyName: string;
-	contactNumber: string;
-	email: string;
-	address: string;
+  name: string;
+  companyName: string;
+  contactNumber: string;
+  email: string;
+  address: string;
 }
--- src/pages/(tenant)/people/pages/suppliers/components/supplier_details/ViewSupplierDetails.tsx
+++ src/pages/(tenant)/people/pages/suppliers/components/supplier_details/ViewSupplierDetails.tsx
@@ -1,79 +1,93 @@
-import ViewDashboardLayout from '@/_app/common/layouts/ViewDashboard';
-import { Supplier } from '@/_app/graphql-models/graphql';
-import { NavLink, Paper, Text, Title } from '@mantine/core';
+import ViewDashboardLayout from "@/_app/common/layouts/ViewDashboard";
+import { Supplier } from "@/_app/graphql-models/graphql";
+import { NavLink, Paper, Text, Title } from "@mantine/core";
 import {
-	IconShoppingCart,
-	IconTruckReturn,
-	IconUserBolt,
-} from '@tabler/icons-react';
-import { useState } from 'react';
-import SupplierDetailsBasicInfo from '../SupplierDetailsBasicInfo';
-import SupplierDetailsPurchase from './SupplierDetailsPurchase';
+	IconPaperclip,
+  IconShoppingCart,
+  IconTruckReturn,
+  IconUserBolt,
+} from "@tabler/icons-react";
+import { useState } from "react";
+import SupplierDetailsBasicInfo from "../SupplierDetailsBasicInfo";
+import SupplierDetailsPurchase from "./SupplierDetailsPurchase";
+import SupplierDetailsDocuments from "./SupplierDetailsDocuments";
 
 interface ISupplierDetailsFormProps {
-	supplierDetails: Supplier | null;
-	refetch: () => void;
+  supplierDetails: Supplier | null;
+  refetch: () => void;
 }
 
 const ViewSupplierDetails: React.FC<ISupplierDetailsFormProps> = ({
-	supplierDetails,
-	refetch,
+  supplierDetails,
+  refetch,
 }) => {
-	// console.log({ supplierDetails });
-	const [activeTab, setActiveTab] = useState<number>(0);
-	return (
-		<div>
-			<ViewDashboardLayout
-				TopSection={
-					<Paper shadow='sm' p={'sm'} withBorder>
-						<Title order={3}>{supplierDetails?.name}</Title>
-						<Text>{supplierDetails?.companyName}</Text>
-					</Paper>
-				}
-				NavSection={
-					<>
-						<NavLink
-							label={'Basic Information'}
-							icon={<IconUserBolt size={16} />}
-							onClick={() => setActiveTab(0)}
-							active={activeTab === 0}
-						/>
-						<NavLink
-							label={'Purchase'}
-							icon={<IconShoppingCart size={16} />}
-							onClick={() => setActiveTab(1)}
-							active={activeTab === 1}
-						/>
-						<NavLink
-							label={'Return'}
-							icon={<IconTruckReturn size={16} />}
-							onClick={() => setActiveTab(2)}
-							active={activeTab === 2}
-						/>
-					</>
-				}
-			>
-				{activeTab === 0 && (
-					<SupplierDetailsBasicInfo
-						supplierDetails={supplierDetails}
-						refetch={() => refetch()}
-					/>
-				)}
-				{activeTab === 1 && (
-					<SupplierDetailsPurchase
-						supplierDetails={supplierDetails}
-						// refetch={refetch}
-					/>
-				)}
-				{activeTab === 2 && (
-					<SupplierDetailsBasicInfo
-						supplierDetails={supplierDetails}
-						refetch={() => refetch()}
-					/>
-				)}
-			</ViewDashboardLayout>
-		</div>
-	);
+  // console.log({ supplierDetails });
+  const [activeTab, setActiveTab] = useState<number>(0);
+  return (
+    <div>
+      <ViewDashboardLayout
+        TopSection={
+          <Paper shadow="sm" p={"sm"} withBorder>
+            <Title order={3}>{supplierDetails?.name}</Title>
+            <Text>{supplierDetails?.companyName}</Text>
+          </Paper>
+        }
+        NavSection={
+          <>
+            <NavLink
+              label={"Basic Information"}
+              icon={<IconUserBolt size={16} />}
+              onClick={() => setActiveTab(0)}
+              active={activeTab === 0}
+            />
+            <NavLink
+              label={"Purchase"}
+              icon={<IconShoppingCart size={16} />}
+              onClick={() => setActiveTab(1)}
+              active={activeTab === 1}
+            />
+            <NavLink
+              label={"Return"}
+              icon={<IconTruckReturn size={16} />}
+              onClick={() => setActiveTab(2)}
+              active={activeTab === 2}
+            />
+            <NavLink
+              label={"Documents"}
+              icon={<IconPaperclip size={16} />}
+              onClick={() => setActiveTab(3)}
+              active={activeTab === 3}
+            />
+          </>
+        }
+      >
+        {activeTab === 0 && (
+          <SupplierDetailsBasicInfo
+            supplierDetails={supplierDetails}
+            // refetch={() => refetch()}
+          />
+        )}
+        {activeTab === 1 && (
+          <SupplierDetailsPurchase
+            supplierDetails={supplierDetails}
+            // refetch={refetch}
+          />
+        )}
+        {activeTab === 2 && (
+          <SupplierDetailsBasicInfo
+            supplierDetails={supplierDetails}
+            // refetch={() => refetch()}
+          />
+        )}
+        {activeTab === 3 && (
+          <SupplierDetailsDocuments
+            supplierDetails={supplierDetails}
+            refetch={() => refetch()}
+          />
+        )}
+      </ViewDashboardLayout>
+    </div>
+  );
 };
 
 export default ViewSupplierDetails;
