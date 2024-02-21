d70cf2ef91485f0c103862adfce3d9ec6d5d1bc6
MHRafix <fmhrafiz27@gmail.com>
2024-02-21 11:46:18 +0600

    save

--- src/pages/(tenant)/inventory-management/inventory.navlinks.tsx
+++ src/pages/(tenant)/inventory-management/inventory.navlinks.tsx
@@ -1,58 +1,64 @@
-import { AppNavLink } from "@/_app/models/AppNavLink.type";
+import { AppNavLink } from '@/_app/models/AppNavLink.type';
 import {
-  IconBrandProducthunt,
-  IconCoin,
-  IconShoppingCart,
-} from "@tabler/icons-react";
+	IconBrandProducthunt,
+	IconCoin,
+	IconShoppingBag,
+	IconShoppingCart,
+} from '@tabler/icons-react';
 
 export const inventoryNavlinks: AppNavLink[] = [
-  {
-    label: "Products",
-    icon: <IconBrandProducthunt />,
-    href: "products",
-    children: [
-      {
-        label: "Products List",
-        href: "products-list",
-      },
-      {
-        label: "Products Category",
-        href: "products-category",
-      },
-      {
-        label: "Barcode",
-        href: "barcode",
-      },
-    ],
-  },
-  {
-    label: "Purchases",
-    icon: <IconShoppingCart />,
-    href: "purchases",
-    children: [
-      {
-        label: "Purchases List",
-        href: "",
-      },
-      // {
-      //   label: "Purchases List",
-      //   href: "purchases-list",
-      // },
-      {
-        label: "Return",
-        href: "return",
-      },
-    ],
-  },
-  {
-    label: "Payments",
-    icon: <IconCoin />,
-    href: "payments",
-    children: [
-      {
-        label: "Supplier Pay",
-        href: "supplier-payments",
-      },
-    ],
-  },
+	{
+		label: 'Products',
+		icon: <IconBrandProducthunt />,
+		href: 'products',
+		children: [
+			{
+				label: 'Products List',
+				href: 'products-list',
+			},
+			{
+				label: 'Products Category',
+				href: 'products-category',
+			},
+			{
+				label: 'Barcode',
+				href: 'barcode',
+			},
+		],
+	},
+	{
+		label: 'Sales',
+		icon: <IconShoppingBag />,
+		href: 'sales',
+	},
+	{
+		label: 'Purchases',
+		icon: <IconShoppingCart />,
+		href: 'purchases',
+		children: [
+			{
+				label: 'Purchases List',
+				href: '',
+			},
+			// {
+			//   label: "Purchases List",
+			//   href: "purchases-list",
+			// },
+			{
+				label: 'Return',
+				href: 'return',
+			},
+		],
+	},
+	{
+		label: 'Payments',
+		icon: <IconCoin />,
+		href: 'payments',
+		children: [
+			{
+				label: 'Supplier Pay',
+				href: 'supplier-payments',
+			},
+		],
+	},
 ];
