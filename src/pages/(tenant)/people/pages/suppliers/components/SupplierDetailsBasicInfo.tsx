f6173a8881f0f29dd8af27809f6a28d2b06a2f40
NishatFariza <nishatfariza012@gmail.com>
2024-03-03 15:46:43 +0600

    Remove Supplier details date info

--- src/pages/(tenant)/people/pages/suppliers/components/SupplierDetailsBasicInfo.tsx
+++ src/pages/(tenant)/people/pages/suppliers/components/SupplierDetailsBasicInfo.tsx
@@ -1,6 +1,6 @@
 import { Supplier } from "@/_app/graphql-models/graphql";
 import { Divider, Paper, Text, Title } from "@mantine/core";
-import { IconBrandBadoo, IconNote } from "@tabler/icons-react";
+import { IconNote } from "@tabler/icons-react";
 import dayjs from "dayjs";
 
 interface ISupplierDetailsProps {
@@ -58,33 +58,6 @@ const SupplierDetailsBasicInfo: React.FC<ISupplierDetailsProps> = ({
               Address:
             </Title>
             <Text>{supplierDetails?.address}</Text>
-          </div>
-        </div>
-      </Paper>
-      <Paper shadow="md" p={"lg"}>
-        <div className="flex items-center justify-between gap-4">
-          <div className="flex items-center gap-4">
-            <IconBrandBadoo size={24} />
-            <Title order={3}>Others</Title>
-          </div>
-        </div>
-        <Divider my="sm" />
-        <div className="flex flex-col w-5/12 gap-3">
-          <div className="flex justify-between">
-            <Title color="gray" order={4}>
-              Appointment Date:
-            </Title>
-            <Text>
-              {dayjs(supplierDetails?.createdAt).format("MMMM D, YYYY h:mm A")}
-            </Text>
-          </div>
-          <div className="flex justify-between">
-            <Title color="gray" order={4}>
-              Joining Date:
-            </Title>
-            <Text>
-              {dayjs(supplierDetails?.updatedAt).format("MMMM D, YYYY h:mm A")}
-            </Text>
           </div>
         </div>
       </Paper>
