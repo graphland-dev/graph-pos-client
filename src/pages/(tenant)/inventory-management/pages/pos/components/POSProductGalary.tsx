f261c323b539d523582b94be5bf3db2dfedb6f68
MHRafix <fmhrafiz27@gmail.com>
2024-03-09 22:40:06 +0600

    save product filter done

--- src/pages/(tenant)/inventory-management/pages/pos/components/POSProductGalary.tsx
+++ src/pages/(tenant)/inventory-management/pages/pos/components/POSProductGalary.tsx
@@ -1,122 +1,160 @@
-import { getFileUrl } from "@/_app/common/utils/getFileUrl";
+import { getFileUrl } from '@/_app/common/utils/getFileUrl';
 import {
-  Product,
-  ProductItemReference,
-  ProductsWithPagination,
-  ServerFileReference,
-} from "@/_app/graphql-models/graphql";
-import { useQuery } from "@apollo/client";
+	BrandsWithPagination,
+	MatchOperator,
+	Product,
+	ProductCategorysWithPagination,
+	ProductItemReference,
+	ProductsWithPagination,
+	ServerFileReference,
+} from '@/_app/graphql-models/graphql';
+import { useQuery } from '@apollo/client';
 import {
-  Badge,
-  Input,
-  Paper,
-  Select,
-  Skeleton,
-  Space,
-  Text,
-} from "@mantine/core";
-import React from "react";
-import { Pos_Products_Query } from "../utils/query.pos";
-import { getProductReferenceByQuantity } from "../utils/utils.calc";
+	Badge,
+	Input,
+	Paper,
+	Select,
+	Skeleton,
+	Space,
+	Text,
+} from '@mantine/core';
+import React, { useState } from 'react';
+import { getSelectInputData } from '../../products/product-edit/components/AssignmentForm';
+import {
+	Pos_Brands_Query,
+	Pos_Categories_Query,
+	Pos_Products_Query,
+} from '../utils/query.pos';
+import { getProductReferenceByQuantity } from '../utils/utils.calc';
 
 interface IProp {
-  onSelectProduct: (product: ProductItemReference) => void;
+	onSelectProduct: (product: ProductItemReference) => void;
 }
 
-const POSProductGalary: React.FC<IProp> = ({ onSelectProduct }) => {
-  const { data: products, loading: productsFetching } = useQuery<{
-    inventory__products: ProductsWithPagination;
-  }>(Pos_Products_Query, {
-    variables: {
-      where: { limit: 20 },
-    },
-  });
+const POSProductGlary: React.FC<IProp> = ({ onSelectProduct }) => {
+	const [category, setCategory] = useState('');
+	const [brand, setBrand] = useState('');
 
-  // categories query
-  // const { data: categories, loading: loadingCategories } = useQuery<{
-  //   inventory__productCategories: ProductCategorysWithPagination;
-  // }>(Pos_Categories_Query);
+	// products fetching
+	const { data: products, loading: productsFetching } = useQuery<{
+		inventory__products: ProductsWithPagination;
+	}>(Pos_Products_Query, {
+		variables: {
+			where: {
+				limit: 20,
+				filters: [
+					{
+						key: 'category',
+						operator: MatchOperator.Eq,
+						value: category,
+					},
+					{
+						key: 'brand',
+						operator: MatchOperator.Eq,
+						value: brand,
+					},
+				],
+			},
+		},
+	});
 
-  // // brands query
-  // const { data: brands, loading: loadingBrands } = useQuery<{
-  //   setup__brands: BrandsWithPagination;
-  // }>(Pos_Brands_Query);
+	// categories query
+	const { data: categories, loading: loadingCategories } = useQuery<{
+		inventory__productCategories: ProductCategorysWithPagination;
+	}>(Pos_Categories_Query);
 
-  const handleEmitProduct = (product: Product) => {
-    const audio = new Audio("/beep.mp3");
-    audio.play();
-    onSelectProduct(getProductReferenceByQuantity(product, 1));
-  };
+	// // brands query
+	const { data: brands, loading: loadingBrands } = useQuery<{
+		setup__brands: BrandsWithPagination;
+	}>(Pos_Brands_Query);
 
-  return (
-    <Paper p={15} className=" h-[calc(100vh-44px)] overflow-y-auto">
-      <div className="grid grid-cols-2 gap-2">
-        <Input.Wrapper size="md">
-          <Select
-            size="md"
-            radius={0}
-            placeholder="Select a category"
-            data={[]}
-          />
-        </Input.Wrapper>
+	// handle emit product
+	const handleEmitProduct = (product: Product) => {
+		const audio = new Audio('/beep.mp3');
+		audio.play();
+		onSelectProduct(getProductReferenceByQuantity(product, 1));
+	};
 
-        <Input.Wrapper size="md">
-          <Select size="md" radius={0} placeholder="Select a brand" data={[]} />
-        </Input.Wrapper>
-      </div>
+	return (
+		<Paper p={15} className=' h-[calc(100vh-44px)] overflow-y-auto'>
+			<div className='grid grid-cols-2 gap-2'>
+				<Input.Wrapper size='md'>
+					<Select
+						size='md'
+						radius={0}
+						placeholder='Select a category'
+						data={getSelectInputData(
+							categories?.inventory__productCategories?.nodes
+						)}
+						onChange={(e) => setCategory(e!)}
+						disabled={loadingCategories}
+					/>
+				</Input.Wrapper>
 
-      <Space h={20} />
-      <div className="grid grid-cols-4 gap-2">
-        {products?.inventory__products?.nodes?.map(
-          (product: Product, idx: number) => (
-            <Paper
-              key={idx}
-              radius={5}
-              shadow="sm"
-              pos={"relative"}
-              onClick={() => handleEmitProduct(product)}
-              className="overflow-hidden border cursor-pointer border-neutral-muted hover:border-blue-500"
-            >
-              <Badge
-                pos={"absolute"}
-                top={0}
-                left={0}
-                radius={0}
-                size="lg"
-                variant="filled"
-              >
-                {product.price} BDT
-              </Badge>
-              <img
-                src={getFileUrl(product?.thumbnail as ServerFileReference)}
-                alt="product image"
-                className="object-cover p-2 rounded-md"
-              />
+				<Input.Wrapper size='md'>
+					<Select
+						size='md'
+						radius={0}
+						placeholder='Select a brand'
+						data={getSelectInputData(brands?.setup__brands?.nodes)}
+						onChange={(e) => setBrand(e!)}
+						disabled={loadingBrands}
+					/>
+				</Input.Wrapper>
+			</div>
 
-              <Space h={5} />
+			<Space h={20} />
+			<div className='grid grid-cols-4 gap-2'>
+				{products?.inventory__products?.nodes?.map(
+					(product: Product, idx: number) => (
+						<Paper
+							key={idx}
+							radius={5}
+							shadow='sm'
+							pos={'relative'}
+							onClick={() => handleEmitProduct(product)}
+							className='overflow-hidden border cursor-pointer border-neutral-muted hover:border-blue-500'
+						>
+							<Badge
+								pos={'absolute'}
+								top={0}
+								left={0}
+								radius={0}
+								size='lg'
+								variant='filled'
+							>
+								{product.price} BDT
+							</Badge>
+							<img
+								src={getFileUrl(product?.thumbnail as ServerFileReference)}
+								alt='product image'
+								className='object-cover p-2 rounded-md'
+							/>
 
-              <div className="p-2">
-                <Text size="xs" fw={500}>
-                  {product?.code}
-                </Text>
-                <Text fz={"md"} fw={500}>
-                  {product?.name}
-                </Text>
-              </div>
-            </Paper>
-          )
-        )}
+							<Space h={5} />
 
-        {productsFetching && (
-          <>
-            {new Array(12).fill(12).map((_, idx) => (
-              <Skeleton key={idx} radius={5} h={200} />
-            ))}
-          </>
-        )}
-      </div>
-    </Paper>
-  );
+							<div className='p-2'>
+								<Text size='xs' fw={500}>
+									{product?.code}
+								</Text>
+								<Text fz={'md'} fw={500}>
+									{product?.name}
+								</Text>
+							</div>
+						</Paper>
+					)
+				)}
+
+				{productsFetching && (
+					<>
+						{new Array(12).fill(12).map((_, idx) => (
+							<Skeleton key={idx} radius={5} h={200} />
+						))}
+					</>
+				)}
+			</div>
+		</Paper>
+	);
 };
 
-export default POSProductGalary;
+export default POSProductGlary;
--- src/pages/(tenant)/inventory-management/pages/pos/components/ProductSelectArea.tsx
+++ src/pages/(tenant)/inventory-management/pages/pos/components/ProductSelectArea.tsx
@@ -1,50 +1,87 @@
-import { Product, ProductItemReference } from "@/_app/graphql-models/graphql";
-import { ErrorMessage } from "@hookform/error-message";
-import { Autocomplete, Input } from "@mantine/core";
-import React from "react";
+import {
+	MatchOperator,
+	Product,
+	ProductItemReference,
+	ProductsWithPagination,
+} from '@/_app/graphql-models/graphql';
+import { useLazyQuery } from '@apollo/client';
+import { ErrorMessage } from '@hookform/error-message';
+import { Autocomplete, Input } from '@mantine/core';
+import { useDebouncedState } from '@mantine/hooks';
+import React, { useEffect, useState } from 'react';
+import { Pos_Products_Query } from '../utils/query.pos';
 
 const ProductSelectArea: React.FC<{
-  formInstance: any;
-  onSelectProduct: (productReference: ProductItemReference) => void;
-  products: Product[];
-  fetchingProducts: boolean;
-}> = ({ formInstance, onSelectProduct, products, fetchingProducts }) => {
-  console.log(onSelectProduct);
-  return (
-    <Input.Wrapper
-      size="md"
-      error={
-        <ErrorMessage name="products" errors={formInstance.formState.errors} />
-      }
-    >
-      <Autocomplete
-        size="md"
-        radius={0}
-        className="w-full"
-        disabled={fetchingProducts}
-        data={getProductSelectInputData(products)}
-        placeholder="Item name/code"
-        onChange={(text) => {
-          console.log(text);
-        }}
-        nothingFound
-      />
-    </Input.Wrapper>
-  );
+	formInstance: any;
+	onSelectProduct: (productReference: ProductItemReference) => void;
+	products: Product[];
+	fetchingProducts: boolean;
+}> = ({ formInstance }) => {
+	const [value, setValue] = useDebouncedState('', 200);
+	const [inputData, setInputData] = useState<any[]>([]);
+
+	// fetch products
+	const [fetchProducts, { data: products }] = useLazyQuery<{
+		inventory__products: ProductsWithPagination;
+	}>(Pos_Products_Query, {
+		variables: {
+			where: {
+				filters: [
+					{
+						key: 'name',
+						operator: MatchOperator.Contains,
+						value,
+					},
+				],
+			},
+		},
+	});
+
+	// fetch based on search
+	useEffect(() => {
+		console.log({ products: inputData });
+		fetchProducts();
+		setInputData(
+			getProductSelectInputData(products?.inventory__products?.nodes ?? [])
+		);
+	}, [value]);
+
+	// console.log(onSelectProduct);
+
+	return (
+		<Input.Wrapper
+			size='md'
+			error={
+				<ErrorMessage name='products' errors={formInstance.formState.errors} />
+			}
+		>
+			<Autocomplete
+				size='md'
+				radius={0}
+				className='w-full'
+				// disabled={productsFetching}
+				data={inputData}
+				// data={[]}
+				placeholder='Item name/code'
+				onChange={(event) => setValue(event)}
+				nothingFound={true}
+			/>
+		</Input.Wrapper>
+	);
 };
 
 export default ProductSelectArea;
 
 const getProductSelectInputData = (data: any) => {
-  const productNames: any = [];
+	const productNames: any = [];
 
-  data?.map((d: any) =>
-    productNames.push({
-      label: d.name,
-      data: { ...d },
-      value: d._id,
-    })
-  );
+	data?.map((d: any) =>
+		productNames.push({
+			label: d.name,
+			data: { ...d },
+			value: d._id,
+		})
+	);
 
-  return productNames;
+	return productNames;
 };
