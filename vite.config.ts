1f96452d97eb71a9fd7c9ba8e310685978a11b93
kingrayhan <rayhan095@gmail.com>
2024-09-14 10:18:11 +0600

    change server config for monolith deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,31 @@
-import { defineConfig } from "vite";
-import * as path from "path";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
-    strictPort: true,
-  },
+  // plugins: [react(), VitePWA()],
   resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4001',
+        changeOrigin: true,
+      },
+      '/socket': {
+        target: 'http://localhost:4001',
+        changeOrigin: true,
+      },
+      '/graphql': {
+        target: 'http://localhost:4001', // GraphQL endpoint
+        changeOrigin: true,
+      },
+    },
   },
 });
