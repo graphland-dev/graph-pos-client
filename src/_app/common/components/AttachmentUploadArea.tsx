5874843b0a56d49518169fd5d602c1b9290b68f9
kingrayhan <rayhan095@gmail.com>
2024-09-15 16:33:29 +0600

    fix attactmentUploadArea prefix type error

--- src/_app/common/components/AttachmentUploadArea.tsx
+++ src/_app/common/components/AttachmentUploadArea.tsx
@@ -6,8 +6,8 @@ import {
   Payroll,
   ServerFileReference,
   Supplier,
-} from "@/_app/graphql-models/graphql";
-import { useServerFile } from "@/_app/hooks/use-upload-file";
+} from '@/_app/graphql-models/graphql';
+import { useServerFile } from '@/_app/hooks/use-upload-file';
 import {
   ActionIcon,
   Button,
@@ -19,16 +19,16 @@ import {
   Text,
   Title,
   rem,
-} from "@mantine/core";
-import { Dropzone, IMAGE_MIME_TYPE } from "@mantine/dropzone";
+} from '@mantine/core';
+import { Dropzone, IMAGE_MIME_TYPE } from '@mantine/dropzone';
 import {
   IconDownload,
   IconFile,
   IconFiles,
   IconUpload,
   IconX,
-} from "@tabler/icons-react";
-import { useState } from "react";
+} from '@tabler/icons-react';
+import { useState } from 'react';
 
 interface IAttachmentUploadProps {
   details: Supplier | Employee | Client | EmployeeDepartment | Payroll | null;
@@ -52,7 +52,7 @@ const AttachmentUploadArea: React.FC<IAttachmentUploadProps> = ({
   const { uploadFile, uploading } = useServerFile();
 
   return (
-    <Paper shadow="md" p={"lg"}>
+    <Paper shadow="md" p={'lg'}>
       <div className="flex items-center justify-between gap-4">
         <div className="flex items-center gap-4">
           <IconFiles size={24} />
@@ -60,7 +60,7 @@ const AttachmentUploadArea: React.FC<IAttachmentUploadProps> = ({
         </div>
       </div>
       <Divider my="sm" />
-      <div className={isGridStyle ? "grid gap-8" : "grid grid-cols-2 gap-3"}>
+      <div className={isGridStyle ? 'grid gap-8' : 'grid grid-cols-2 gap-3'}>
         <div>
           {details?.attachments?.length ? (
             <>
@@ -75,7 +75,7 @@ const AttachmentUploadArea: React.FC<IAttachmentUploadProps> = ({
                 >
                   <div className="flex items-center gap-2">
                     <IconFile color="teal" size={24} />
-                    <Text fw={500} fz={"xs"} className="line-clamp-1">
+                    <Text fw={500} fz={'xs'} className="line-clamp-1">
                       {attachment?.path}
                     </Text>
                   </div>
@@ -87,29 +87,27 @@ const AttachmentUploadArea: React.FC<IAttachmentUploadProps> = ({
                     <ActionIcon
                       color="red"
                       variant="filled"
-                      size={"sm"}
+                      size={'sm'}
                       onClick={() => {
                         const restAttachments = details?.attachments?.filter(
-                          (attach) => attach?.path !== attachment?.path
+                          (attach) => attach?.path !== attachment?.path,
                         );
                         // console.log(restAttachments);
 
                         updateAttachmentsMutation({
                           variables: {
                             where: {
-                              key: "_id",
+                              key: '_id',
                               operator: MatchOperator.Eq,
                               value: details?._id,
                             },
                             body: {
                               attachments:
-                                [
-                                  restAttachments?.map((att) => ({
-                                    meta: att?.meta,
-                                    path: att?.path,
-                                    provider: att?.provider,
-                                  })) as ServerFileReference[],
-                                ] ?? [],
+                                (restAttachments?.map((att) => ({
+                                  meta: att?.meta,
+                                  path: att?.path,
+                                  provider: att?.provider,
+                                })) as ServerFileReference[]) ?? [],
                             },
                           },
                         });
@@ -128,20 +126,20 @@ const AttachmentUploadArea: React.FC<IAttachmentUploadProps> = ({
                 No documents found!
               </Title>
             </div>
-          )}{" "}
+          )}{' '}
         </div>
 
         {isGridStyle && <Divider h={1} />}
 
         <div
-          className={isGridStyle ? "pl-5" : "border-l-[2px] border-solid  pl-5"}
+          className={isGridStyle ? 'pl-5' : 'border-l-[2px] border-solid  pl-5'}
         >
           <Input.Wrapper size="md" label="Upload attachments">
             <Dropzone
               onDrop={(files) => {
                 setAttachments((prev) => [...prev!, ...files]);
               }}
-              onReject={(files) => console.log("rejected files", files)}
+              onReject={(files) => console.log('rejected files', files)}
               maxSize={5 * 1024 ** 2}
               accept={IMAGE_MIME_TYPE}
               className="flex items-center justify-center"
@@ -151,7 +149,7 @@ const AttachmentUploadArea: React.FC<IAttachmentUploadProps> = ({
                   style={{
                     width: rem(42),
                     height: rem(42),
-                    color: "var(--mantine-color-blue-6)",
+                    color: 'var(--mantine-color-blue-6)',
                   }}
                   color="teal"
                   stroke={1.5}
@@ -160,7 +158,7 @@ const AttachmentUploadArea: React.FC<IAttachmentUploadProps> = ({
             </Dropzone>
           </Input.Wrapper>
 
-          <Space h={"md"} />
+          <Space h={'md'} />
 
           {Boolean(attachments?.length) && (
             <div>
@@ -179,7 +177,7 @@ const AttachmentUploadArea: React.FC<IAttachmentUploadProps> = ({
                   <div className="flex items-center justify-between w-full">
                     <div className="flex items-center gap-2">
                       <IconFile color="teal" size={24} />
-                      <Text fw={500} fz={"xs"} className="line-clamp-1">
+                      <Text fw={500} fz={'xs'} className="line-clamp-1">
                         {attachment?.name}
                       </Text>
                     </div>
@@ -198,7 +196,7 @@ const AttachmentUploadArea: React.FC<IAttachmentUploadProps> = ({
                 </Paper>
               ))}
 
-              <Space h={"sm"} />
+              <Space h={'sm'} />
               <Group position="right">
                 <Button
                   color="orange"
@@ -211,20 +209,19 @@ const AttachmentUploadArea: React.FC<IAttachmentUploadProps> = ({
                     updateAttachmentsMutation({
                       variables: {
                         where: {
-                          key: "_id",
+                          key: '_id',
                           operator: MatchOperator.Eq,
                           value: details?._id,
                         },
                         body: {
-                          attachments:
-                            [
-                              ...(details?.attachments?.map((att) => ({
-                                meta: att?.meta,
-                                path: att?.path,
-                                provider: att?.provider,
-                              })) as ServerFileReference[]),
-                              ...(res?.data as ServerFileReference[]),
-                            ] ?? [],
+                          attachments: [
+                            ...(details?.attachments?.map((att) => ({
+                              meta: att?.meta,
+                              path: att?.path,
+                              provider: att?.provider,
+                            })) as ServerFileReference[]),
+                            ...(res?.data as ServerFileReference[]),
+                          ],
                         },
                       },
                     });
