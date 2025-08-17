3b5adf8d8f3ebcf28c35c49640d46af770576f19
kingrayhan <rayhan095@gmail.com>
2025-08-17 22:23:38 +0600

    feat: Remove MegaMenuTrigger export from MegaMenu component

--- src/commons/components/MegaMenu/index.ts
+++ src/commons/components/MegaMenu/index.ts
@@ -1,4 +1,3 @@
 export { MegaMenu } from "./MegaMenu";
 export { MenuSection } from "./MenuSection";
 export { QuickActions } from "./QuickActions";
-export { MegaMenuTrigger } from "./MegaMenuTrigger";
